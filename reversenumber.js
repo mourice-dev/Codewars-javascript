699132c9991a0337e6f80945114b7715a5f50aea
Nshuti Maurice <nshutikope@gmail.com>
2026-01-20 12:00:00 +0200

    Add solution for reversenumber kata

---
+++ reversenumber.js
@@ -0,0 +1,20 @@
+// Given a number, write a function to output its reverse digits. (e.g. given 123 the answer is 321)
+
+// Numbers should preserve their sign; i.e. a negative number should still be negative when reversed.
+
+// Examples
+//  123 ->  321
+// -456 -> -654
+// 1000 ->    1
+
+function reverseNumber(n) {
+  const x = String(n);
+  const b = x.split("");
+
+  if (Math.max(...b) === Number(b[0])) {
+    return Number(n);
+  } else {
+    return b.reverse().join("");
+  }
+}
+console.log(reverseNumber("321"));
