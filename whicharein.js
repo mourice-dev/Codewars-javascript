a6ee4a29dfffd7553d652a287fa4697725f39630
nshuti <nshutikope@gmail.com>
2026-09-21 12:00:00 -0400

    Solve which are in kata

--- whicharein.js
+++ whicharein.js
@@ -19,3 +19,9 @@
 // Arrays are written in "general" notation. See "Your Test Cases" for examples in your language.
 // In Shell bash a1 and a2 are strings. The return is a string where words are separated by commas.
 // Beware: In some languages r must be without duplicates.
+
+function inArray(array1, array2) {
+  return array1
+    .filter((candidate) => array2.some((value) => value.includes(candidate)))
+    .sort();
+}
